58367fc9c25c1c56c719de945aa9da1d48a4a229
itsaimsd <madhusudantiwari2003@gmail.com>
2025-01-12 21:07:43 +0530

    route

--- src/app.js
+++ src/app.js
@@ -2,24 +2,31 @@ const express = require("express");
 
 const app = express();
 
-// app.use("/route", rH, [rH2, rH3], rH4, rH5);
+// GET / User =>middleware chain => request handlet
+
+
+app.use("/", (req, res, next) => {
+  res.send("Handling / route");
+  next();
+});
 
 app.use(
   "/user",
-  [
-    (req, res, next) => {
-      console.log("Handling the route user!!");
-      // res.send("1st response");
-      next();
-    },
-    (req, res, next) => {
-      console.log("Handling the route user 2!!");
-      // res.send(" 2nd response");
-      next();
-    },
-  ],
+
+  (req, res, next) => {
+    console.log("Handling the route user!!");
+    // res.send("1st response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 2!!");
+    // res.send(" 2nd response");
+    next();
+  },
+
   (req, res, next) => {
     console.log("Handling the route user 3!!");
+    console.log("This is request handler because it send request");
     res.send(" 3rd response");
     // next();
   }

ce2c59bfd6af8d49553f683eca8184a179025161
itsaimsd <madhusudantiwari2003@gmail.com>
2025-01-13 02:11:29 +0530

    Middleware use

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ const app = express();
 
 // GET / User =>middleware chain => request handlet
 
-
 // app.use("/", (req, res, next) => {
 //   res.send("Handling / route");
 //   next();
@@ -28,14 +27,31 @@ const app = express();
 //     console.log("Handling the route user 3!!");
 //     console.log("This is request handler because it send request");
 //     res.send(" 3rd response");
-//     // next(); 
+//     // next();
 //   }
 // );
 
+app.get("/admin/getAllData", (req, res) => {
+  // Logic of checking if the request is authorized
+  const token = "XYZABCDEF";
+  const isAdminAuthorized = token === "xyz";
+  if (isAdminAuthorized) {
+    res.send("All Data sent");
+  } else {
+    res.status(401).send("unauthorized user");
+  }
+});
 
-
-
-app.get
+app.get("/admin/deleteUser", (req, res) => {
+  // Logic of checking if the request is authorized
+  const token = "XYZABCDEF";
+  const isAdminAuthorized = token === "xyz";
+  if (!isAdminAuthorized) {
+    res.send("user is not authorized");
+  } else {
+    res.status(401).send("unauthorized user");
+  }
+});
 
 app.listen(3000, () => {
   console.log("server is successfully listening on port 3000.....");

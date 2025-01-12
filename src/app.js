192990591b4155462ed18717583d53d479c8c9b9
itsaimsd <madhusudantiwari2003@gmail.com>
2025-01-13 01:30:20 +0530

    Importance of Middleware

--- src/app.js
+++ src/app.js
@@ -5,32 +5,38 @@ const app = express();
 // GET / User =>middleware chain => request handlet
 
 
-app.use("/", (req, res, next) => {
-  res.send("Handling / route");
-  next();
-});
+// app.use("/", (req, res, next) => {
+//   res.send("Handling / route");
+//   next();
+// });
 
-app.use(
-  "/user",
+// app.use(
+//   "/user",
 
-  (req, res, next) => {
-    console.log("Handling the route user!!");
-    // res.send("1st response");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling the route user 2!!");
-    // res.send(" 2nd response");
-    next();
-  },
+//   (req, res, next) => {
+//     console.log("Handling the route user!!");
+//     // res.send("1st response");
+//     next();
+//   },
+//   (req, res, next) => {
+//     console.log("Handling the route user 2!!");
+//     // res.send(" 2nd response");
+//     next();
+//   },
 
-  (req, res, next) => {
-    console.log("Handling the route user 3!!");
-    console.log("This is request handler because it send request");
-    res.send(" 3rd response");
-    // next();
-  }
-);
+//   (req, res, next) => {
+//     console.log("Handling the route user 3!!");
+//     console.log("This is request handler because it send request");
+//     res.send(" 3rd response");
+//     // next(); 
+//   }
+// );
+
+
+
+
+app.get
+
 app.listen(3000, () => {
   console.log("server is successfully listening on port 3000.....");
 });

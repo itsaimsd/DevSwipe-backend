ebdd0b95c865b49f86778d28f4cde7f9373b30a1
itsaimsd <madhusudantiwari2003@gmail.com>
2025-01-14 05:45:58 +0530

    Exploring validator library

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+// database
 const connectDB = async () => {
   await mongoose.connect(
     "mongodb+srv://itsaimsd:c6QlvjSabP804nxP@namastenode.uieky.mongodb.net/devTinder"
